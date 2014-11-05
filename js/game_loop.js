a2570fd6a3505d5ffa80eb07ce20601a987e8cc9
dansamargiu <samargiudan@gmail.com>
2014-11-04 22:18:33 -0500

    Improved game loop to process to have a fixed time step, and variable rendering. Added entities array, added cube to entities array. Game loop now calls update on all items in entities array.

--- js/game_loop.js
+++ js/game_loop.js
@@ -1,12 +1,35 @@
-module.exports.start = function(renderer, camera, scene, cube) {
+var MS_PER_UPDATE = 30;
+var previous = new Date().getTime();
+var lag = 0.0;
+
+module.exports.start = function(render, entities) {
   // Game Loop
-  function render() {
-    requestAnimationFrame(render);
-    // rotate cube
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.05;
-    renderer.render(scene, camera);
+  function game_loop() {
+    requestAnimationFrame(game_loop);
+
+    var current = new Date().getTime();
+    var elapsed = current - previous;
+    previous = current;
+    lag += elapsed;
+
+    // process input
+    // TODO
+
+    while (lag >= MS_PER_UPDATE) {
+      // update
+      console.log("update");
+      for (var i = 0; i < entities.length; i++) {
+        entities[i].update();
+      }
+
+      lag -= MS_PER_UPDATE;
+    }
+
+    // render
+    console.log("render");
+    render(lag / MS_PER_UPDATE);
   }
+
   // Start the loop
-  render();
+  game_loop();
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 var THREE = require('three');
+
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// create a set of coordinate axes to help orient user
+// specify length in pixels in each direction
+var axes = new THREE.AxisHelper(100);
+scene.add(axes);
+
 // Add the cube
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({
@@ -11,8 +19,27 @@ var material = new THREE.MeshBasicMaterial({
 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
 // Move the camera out so it is not in the cube
 camera.position.z = 5;
 
+cube.update = function() {
+  this.rotation.x += 0.01;
+  this.rotation.y += 0.01;
+};
+
+camera.update = function() {
+
+};
+
+var entities = [camera, cube];
+
+function render(leftover) {
+  console.log(leftover);
+  // TODO: use leftover ammount (value = 0 to 1)
+  // to extrapolate animation between frames.
+  renderer.render(scene, camera);
+}
+
 var game_loop = require('./game_loop.js');
-game_loop.start(renderer, camera, scene, cube);
+game_loop.start(render, entities);
