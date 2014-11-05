da7fe01859951e67924ccb31917c81ac4db12ff7
dansamargiu <samargiudan@gmail.com>
2014-11-04 23:16:15 -0500

    Moved init code to another module. Added a different colored mesh for each side of the cube.

---
+++ js/init.js
@@ -0,0 +1,54 @@
+var THREE = require('three');
+
+module.exports = function(world) {
+  world.renderer = new THREE.WebGLRenderer();
+  world.renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(world.renderer.domElement);
+
+  world.scene = new THREE.Scene();
+  world.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  // create a set of coordinate axes to help orient user
+  // specify length in pixels in each direction
+  world.scene.add(new THREE.AxisHelper(100));
+
+  // Create an array of materials to be used in a cube, one for each side
+  var cubeMaterialArray = [];
+  // order to add materials: x+,x-,y+,y-,z+,z-
+  cubeMaterialArray.push(new THREE.MeshBasicMaterial({
+    color: 0xff3333
+  }));
+  cubeMaterialArray.push(new THREE.MeshBasicMaterial({
+    color: 0xff8800
+  }));
+  cubeMaterialArray.push(new THREE.MeshBasicMaterial({
+    color: 0xffff33
+  }));
+  cubeMaterialArray.push(new THREE.MeshBasicMaterial({
+    color: 0x33ff33
+  }));
+  cubeMaterialArray.push(new THREE.MeshBasicMaterial({
+    color: 0x3333ff
+  }));
+  cubeMaterialArray.push(new THREE.MeshBasicMaterial({
+    color: 0x8833ff
+  }));
+  var cubeMaterials = new THREE.MeshFaceMaterial(cubeMaterialArray);
+  var cubeGeometry = new THREE.CubeGeometry(1, 1, 1);
+  var cube = new THREE.Mesh(cubeGeometry, cubeMaterials);
+  world.scene.add(cube);
+
+  // Move the camera out so it is not in the cube
+  world.camera.position.z = 5;
+
+  cube.update = function() {
+    this.rotation.x += 0.01;
+    this.rotation.y += 0.01;
+  };
+
+  world.camera.update = function() {
+
+  };
+
+  world.entities = [world.camera, cube];
+}
--- js/main.js
+++ js/main.js
@@ -1,45 +1,16 @@
-var THREE = require('three');
-
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// create a set of coordinate axes to help orient user
-// specify length in pixels in each direction
-var axes = new THREE.AxisHelper(100);
-scene.add(axes);
-
-// Add the cube
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00
-});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-// Move the camera out so it is not in the cube
-camera.position.z = 5;
-
-cube.update = function() {
-  this.rotation.x += 0.01;
-  this.rotation.y += 0.01;
+var _world = {
+  name: "main"
 };
-
-camera.update = function() {
-
-};
-
-var entities = [camera, cube];
 
 function render(leftover) {
   console.log(leftover);
   // TODO: use leftover ammount (value = 0 to 1)
   // to extrapolate animation between frames.
-  renderer.render(scene, camera);
+  _world.renderer.render(_world.scene, _world.camera);
 }
 
+var init = require('./init.js');
+init(_world);
+
 var game_loop = require('./game_loop.js');
-game_loop.start(render, entities);
+game_loop.start(render, _world.entities);
